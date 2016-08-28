5ca60fa38cf5fe3b2ce696665591cac5ec80467a
Mr. X <imblueiicee@gmail.com>
2016-08-29 01:18:19 +0530

    Update: Session vars and Randomization

--- Quiz2.php
+++ Quiz2.php
@@ -1,6 +1,67 @@
-<!DOCTYPE HTML>
+<?php
+	require_once('sql.php');
+	// Create connection
+			$conn = new mysqli($DBservername, $DBusername, $DBpass, $DBname);
+			// Check connection
+			if ($conn->connect_error) {
+		 	   die("Connection failed: " . $conn->connect_error);
+			} 
+	if(!session_id())
+		session_start();
+
+		// echo session_id();
+		if(!isset($_SESSION['quid'])){
+			$quid = array();
+			
+
+	   		$sql = "SELECT quid FROM questionbank where qzno = 1";
+
+			$retval = $conn->query($sql);
+			if ($retval->num_rows<=0) {
+				echo "No Questions";
+			}
+
+			while($row = $retval->fetch_assoc()){
+				echo "Happ";
+				array_push($quid, $row['quid']);
+			}
+			$_SESSION['quid'] = $quid;
+		}
+
+		shuffle($_SESSION['quid']);
+
+		if(count($_SESSION['quid'])<=0){
+			echo "No more questions, ";
+			echo "Stop the timer.";
+			session_destroy();
+			exit();
+		}
+
+		$randQuid = $_SESSION['quid'][0];
+		unset($_SESSION['quid'][0]);
+		$_SESSION['lastQuid'] = $randQuid;
+		
+		echo "Quid selected : " . $randQuid;
+		echo ", Total Quids left: " . count($_SESSION['quid']);
+
+		$sql = "select * from questionbank where quid = $randQuid";
+		$retval = $conn->query($sql);
+		$row = $retval->fetch_assoc();
+		$_SESSION['lastAns'] = $row['opt1'];
+
+		//POST REQUEST -> CHECK THE ANSWER FOR 'LASTquiz' - 'LASTans'
+
+	// Randomizing Options.
+		$options = array($row['opt1'],$row['opt2'],$row['opt3'],$row['opt4']);
+		shuffle($options)
+
+	// exit();
+	?>
+
+	<!DOCTYPE HTML>
 <html>
 <head>
+
     <meta charset="UTF-8">
 	<title>Quiz 1</title>
 	<link rel="stylesheet" href="css/quiz2style.css">
@@ -28,16 +89,16 @@
         <div class="line">
         </div>
         <div class="labelback">
-            <label>Q1. Choice Question 1.</label>
+            <label><?php echo $row['ques']; ?></label>
         </div>
         
     </div>
     <div class="contentspace">
         <div class="btn">
-        <button type="submit" onclick="Choice1();" id="btn">Possible Answer 1</button>
-        <button type="submit" onclick="Choice2();" id="btn">Possible Answer 2</button>
-        <button type="submit" onclick="Choice3();" id="btn">Possible Answer 3</button>
-        <button type="submit" onclick="Choice4();" id="btn">Possible Answer 4</button></div>
+        <button type="submit" value="<?php echo $options[0]; ?>" onclick="Choice1();" id="btn"><?php echo $options[0]; ?></button>
+        <button type="submit"  value="<?php echo $options[1]; ?>" onclick="Choice2();" id="btn"><?php echo $options[1]; ?></button>
+        <button type="submit"  value="<?php echo $options[2]; ?>" onclick="Choice3();" id="btn"><?php echo $options[2]; ?></button>
+        <button type="submit"  value="<?php echo $options[3]; ?>" onclick="Choice4();" id="btn"><?php echo $options[3]; ?></button></div>
     </div>
 </div>
 </body>
